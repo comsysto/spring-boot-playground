1d8071691d0abbb20107631c06c308bb130e0f89
AmerAljovic <torquiL1945liveseY>
2014-09-07 13:20:50 +0200

    Simplified pom file.

--- spring-boot-sample/src/main/resources/db/migration/V1__init.sql
+++ spring-boot-sample/src/main/resources/db/migration/V1__init.sql
@@ -5,7 +5,5 @@ CREATE TABLE QUOTE (
 
 insert into QUOTE (quote) values ('Working with Spring Boot is like pair-programming with the Spring developers.');
 insert into QUOTE (quote) values ('With Boot you deploy everywhere you can find a JVM basically.');
-insert into QUOTE (quote) values ('Spring has come quite a ways in addressing developer enjoyment and '
-      + 'ease of use since the last time I built an application using it.');
-insert into QUOTE (quote) values ('Previous to Spring Boot, I remember XML hell, confusing set up, and '
-      + 'many hours of frustration.');
+insert into QUOTE (quote) values ('Spring has come quite a ways in addressing developer enjoyment and ease of use since the last time I built an application using it.');
+insert into QUOTE (quote) values ('Previous to Spring Boot, I remember XML hell, confusing set up, and many hours of frustration.');
