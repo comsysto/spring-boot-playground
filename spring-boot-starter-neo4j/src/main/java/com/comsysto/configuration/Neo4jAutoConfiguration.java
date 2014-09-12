eb8c8a471eb70fb46ba8f7070027deb17b81f7eb
AmerAljovic <torquiL1945liveseY>
2014-09-12 17:19:02 +0200

    spring-boot-starter-neo4j: resolved base package problem

--- spring-boot-starter-neo4j/src/main/java/com/comsysto/configuration/Neo4jAutoConfiguration.java
+++ spring-boot-starter-neo4j/src/main/java/com/comsysto/configuration/Neo4jAutoConfiguration.java
@@ -9,28 +9,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 @ConditionalOnClass(Node.class)
-@EnableNeo4jRepositories(basePackages = "com.comsysto.boot.neo4j")
+@EnableNeo4jRepositories (basePackages = "")
 public class Neo4jAutoConfiguration extends Neo4jConfiguration
 {
-    public Neo4jAutoConfiguration()
+    public Neo4jAutoConfiguration() throws ClassNotFoundException
     {
-        setBasePackage("com.comsysto.boot.neo4j");
+        setBasePackage("");
     }
 
     @Bean
     GraphDatabaseService graphDatabaseService() {
         return new GraphDatabaseFactory().newEmbeddedDatabase("accessingdataneo4j.db");
     }
-
-    @PostConstruct
-    public void init()
-    {
-        System.out.println("test");
-    }
-
-
 }
