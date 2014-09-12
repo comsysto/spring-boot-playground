2f2b23cadd048ddca2452a89464a536c175188c1
AmerAljovic <torquiL1945liveseY>
2014-09-12 15:35:08 +0200

    spring-boot-playground-neo4j: Initial autoconfiguration with base package hardcoded. Need to resolve that

--- spring-boot-starter-neo4j/src/main/java/com/comsysto/configuration/Neo4jAutoConfiguration.java
+++ spring-boot-starter-neo4j/src/main/java/com/comsysto/configuration/Neo4jAutoConfiguration.java
@@ -1,17 +1,36 @@
 package com.comsysto.configuration;
 
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @ConditionalOnClass(Node.class)
-public class Neo4jAutoConfiguration
+@EnableNeo4jRepositories(basePackages = "com.comsysto.boot.neo4j")
+public class Neo4jAutoConfiguration extends Neo4jConfiguration
 {
-    @Bean
-    public String testBean()
+    public Neo4jAutoConfiguration()
     {
-        return "test";
+        setBasePackage("com.comsysto.boot.neo4j");
     }
+
+    @Bean
+    GraphDatabaseService graphDatabaseService() {
+        return new GraphDatabaseFactory().newEmbeddedDatabase("accessingdataneo4j.db");
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("test");
+    }
+
+
 }
